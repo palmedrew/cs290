ee5308cbdcbc2f5bdd7f75f86d6dd322bc34b8e9
Andrew Palma <palmaa@onid.oregonstate.edu>
2015-05-09 16:16:04 -0700

    ignoring db_connection.php from git

--- assignment4-part2/src/db_connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-   $dbHost = 'oniddb.cws.oregonstate.edu';
-   $dbName = 'palmaa-db';
-   $dbUser = 'palmaa-db';
-   $dbPass = 'lK2gXlbqKTTlVMrB';  //goood pass
-//   $dbPass = 'AK2gXlbqKTTlVMrB';  //bad pass
-?>
